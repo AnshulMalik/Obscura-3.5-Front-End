007c583dcbe6c177590d326b38e6128b7a7d1f37
Anshul <malikanshul29@gmail.com>
2016-10-14 22:22:58 +0530

    credits

--- src/Components/Credits.react.js
+++ src/Components/Credits.react.js
@@ -5,15 +5,16 @@ var Credits = React.createClass({
         return (
 
             <section className="team"><br/>
-                <center><p><h2>Members</h2></p></center>
+                <center>
+                    <p><h2>Mentors</h2></p>
                 <ul className="ch-grid">
                     <li>
                         <div className="ch-item ch-img-1">
-                            <div className="ch-info-wrap">
+                            <div id="mentors1" className="ch-info-wrap">
                                 <div className="ch-info">
                                     <div className="ch-info-front ch-img-1"></div>
                                     <div className="ch-info-back">
-                                        <center><h3>Dayal Saini</h3><p>Team member</p></center>
+                                        <center><h3>Pranjal Seth</h3><p>9729011510</p></center>
 
                                     </div>
                                 </div>
@@ -22,11 +23,11 @@ var Credits = React.createClass({
                     </li>
                     <li>
                         <div className="ch-item ch-img-1">
-                            <div className="ch-info-wrap">
+                            <div id="mentors2" className="ch-info-wrap">
                                 <div className="ch-info">
                                     <div className="ch-info-front ch-img-1"></div>
                                     <div className="ch-info-back">
-                                        <center><h3>Dayal Saini</h3><p>Team member</p></center>
+                                        <center><h3>Chirantan Das</h3><p>8607488266</p></center>
 
                                     </div>
                                 </div>
@@ -35,11 +36,11 @@ var Credits = React.createClass({
                     </li>
                     <li>
                         <div className="ch-item ch-img-1">
-                            <div className="ch-info-wrap">
+                            <div id="mentors3" className="ch-info-wrap">
                                 <div className="ch-info">
                                     <div className="ch-info-front ch-img-1"></div>
                                     <div className="ch-info-back">
-                                        <center><h3>Dayal Saini</h3><p>Team member</p></center>
+                                        <center><h3>Arijit Goswami</h3><p>9728434068</p></center>
 
                                     </div>
                                 </div>
@@ -48,11 +49,11 @@ var Credits = React.createClass({
                     </li>
                     <li>
                         <div className="ch-item ch-img-1">
-                            <div className="ch-info-wrap">
+                            <div id="mentors4" className="ch-info-wrap">
                                 <div className="ch-info">
                                     <div className="ch-info-front ch-img-1"></div>
                                     <div className="ch-info-back">
-                                        <center><h3>Dayal Saini</h3><p>Team member</p></center>
+                                        <center><h3>Vatsal Garg</h3><p>8950443295</p></center>
 
                                     </div>
                                 </div>
@@ -61,11 +62,30 @@ var Credits = React.createClass({
                     </li>
                     <li>
                         <div className="ch-item ch-img-1">
-                            <div className="ch-info-wrap">
+                            <div id="mentors5" className="ch-info-wrap">
                                 <div className="ch-info">
                                     <div className="ch-info-front ch-img-1"></div>
                                     <div className="ch-info-back">
-                                        <center><h3>Dayal Saini</h3><p>Team member</p></center>
+                                        <center><h3>Prasanna</h3><p>7056519967</p></center>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+
+                </ul>
+
+
+                <center><p><h2>Initiates</h2></p></center>
+                <ul className="ch-grid">
+                    <li>
+                        <div className="ch-item ch-img-1">
+                            <div id="Initiates1" className="ch-info-wrap">
+                                <div className="ch-info">
+                                    <div className="ch-info-front ch-img-1"></div>
+                                    <div className="ch-info-back">
+                                        <center><h3>Rohan Sharma</h3><p>7404127407</p></center>
 
                                     </div>
                                 </div>
@@ -74,11 +94,76 @@ var Credits = React.createClass({
                     </li>
                     <li>
                         <div className="ch-item ch-img-1">
-                            <div className="ch-info-wrap">
+                            <div id="Initiates2" className="ch-info-wrap">
                                 <div className="ch-info">
                                     <div className="ch-info-front ch-img-1"></div>
                                     <div className="ch-info-back">
-                                        <center><h3>Dayal Saini</h3><p>Team member</p></center>
+                                        <center><h3>Vedant nepal</h3><p>9896639653</p></center>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="ch-item ch-img-1">
+                            <div id="Initiates3" className="ch-info-wrap">
+                                <div className="ch-info">
+                                    <div className="ch-info-front ch-img-1"></div>
+                                    <div className="ch-info-back">
+                                        <center><h3>Yash Gupta</h3><p>7404658930</p></center>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="ch-item ch-img-1">
+                            <div id="Initiates4" className="ch-info-wrap">
+                                <div className="ch-info">
+                                    <div className="ch-info-front ch-img-1"></div>
+                                    <div className="ch-info-back">
+                                        <center><h3>Saqib Kamal</h3><p>8059710704</p></center>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="ch-item ch-img-1">
+                            <div id="Initiates5" className="ch-info-wrap">
+                                <div className="ch-info">
+                                    <div className="ch-info-front ch-img-1"></div>
+                                    <div className="ch-info-back">
+                                        <center><h3>Rajan Nagpal</h3><p>9646569743</p></center>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="ch-item ch-img-1">
+                            <div id="Initiates6" className="ch-info-wrap">
+                                <div className="ch-info">
+                                    <div className="ch-info-front ch-img-1"></div>
+                                    <div className="ch-info-back">
+                                        <center><h3>Sachin Jha</h3><p>9896639877</p></center>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="ch-item ch-img-1">
+                            <div id="Initiates7" className="ch-info-wrap">
+                                <div className="ch-info">
+                                    <div className="ch-info-front ch-img-1"></div>
+                                    <div className="ch-info-back">
+                                        <center><h3>Prasant Kumar</h3><p>9472587580</p></center>
 
                                     </div>
                                 </div>
@@ -86,8 +171,39 @@ var Credits = React.createClass({
                         </div>
                     </li>
                 </ul>
+                <center><p><h2>Architects</h2></p></center>
+                <ul className="ch-grid">
+                    <li>
+                        <div className="ch-item ch-img-1">
+                            <div id="architect1" className="ch-info-wrap">
+                                <div className="ch-info">
+                                    <div className="ch-info-front ch-img-1"></div>
+                                    <div className="ch-info-back">
+                                        <center><h3>Anshul Malik</h3><p>9050563712</p></center>
 
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="ch-item ch-img-1">
+                            <div id="architect2" className="ch-info-wrap">
+                                <div className="ch-info">
+                                    <div className="ch-info-front ch-img-1"></div>
+                                    <div className="ch-info-back">
+                                        <center><h3>Dayal Saini</h3><p>7610000894</p></center>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+
+                </ul>
+                </center>
             </section>
+
 
         );
     }
